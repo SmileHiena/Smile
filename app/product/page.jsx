331e5cdd38bec3b4125b40ab6d3a9cd89cf9a80c
SmileHiena <nam22042004@gmail.com>
2024-08-07 17:18:03 +0700

    test

---
+++ app/product/page.jsx
@@ -0,0 +1,49 @@
+import Hot from "../component/Hot";
+import ProductCard from "../component/ProductCard";
+
+
+export const metadata = {
+  title: "Product Shop",
+  description:
+    "Mua đồng hồ nam đẹp, trẻ trung, đồng hồ chất lượng, thiết kế mới, thoải mái từ thương hiệu UNIQLO với nhiều kiểu dáng cùng chất lượng",
+};
+export default async function Product() {
+  const productRes = await fetch("http://localhost:3000/products", {cache: "no-store",});
+  const products = await productRes.json();
+
+  const hotRes = await fetch("http://localhost:3000/products/hot", {cache: "no-store",});
+  const hotProducts = await hotRes.json();
+
+  return (
+    <>
+      <div className="background_bg">
+        <div className="watchs_section layout_padding">
+          <div className="container">
+            <div className="about_section_3">
+              <div className="row">
+                <div className="watchs_taital">
+                    <h1 style={{ color: "white", textAlign: "center" }}>All Products</h1><br/>
+                </div>
+                <ProductCard data={products} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="background_bg">
+        <div className="watchs_section layout_padding">
+          <div className="container">
+            <div className="about_section_3">
+              <div className="row">
+                <div className="watchs_taital">
+                  <h1 style={{ color: "white", textAlign: "center" }}>Hot Products</h1><br/>
+                </div>
+                <Hot data={hotProducts} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
